d39f373bce9035519c7e57594e7a7e226b419ca1
Rowland <rowlandm@rowlandm-desktop.(none)>
2010-11-11 22:53:10 +1000

    Adding a test to export tables to CSV via chrome extension

---
+++ export_table/Quarterback Stats by Team  2010 Opponents' FF Points Allowed, Remaining Schedule - FF Today_files/rollOverColors.js
@@ -0,0 +1,7 @@
+// rollOverColors.js - defines rollOver colors for various page elements
+
+// rollOver Colors for 'Top Experiences' and 'Vacation Ideas'
+var hoverColors = new Array();
+hoverColors['default'] = '#ffffff';
+hoverColors['hover1'] = '#eeeeee';
+
